9975c553876853cdf027989db069ee09d998c3fb
Puja Kusuma <pujakusumae@hotmail.com>
2020-10-02 22:27:42 +0700

    Fix exportedLabel

--- gen-client/src/components/GlobalConfiguration.vue
+++ gen-client/src/components/GlobalConfiguration.vue
@@ -1,52 +1,48 @@
 <template>
-    <div class="row justify-between items-center" style="height: 150px">
-        <div class="col-auto">
-            <q-btn
-                class="q-ma-xs"
-                icon="delete"
-                color="negative"
-                label="Clear all data"
-                :disable="!units.length"
-                @click="clearStore()"
-            />
-        </div>
-        <div class="col-auto">
-            <q-btn
-                class="q-ma-xs"
-                icon="stop"
-                color="primary"
-                label="Ingnore command"
-                @click="ignoreCommand()"
-            />
-        </div>
-        <div class="col-auto">
-            <q-toggle
-                v-model="timeCalibrationState"
-                label="Time Calibration"
-                class="q-ma-xs"
-            />
-        </div>
-        <div class="col-auto">
-            <q-toggle
-                v-model="combineCommand"
-                label="Combine Command"
-                class="q-ma-xs"
-            />
-        </div>
-        <div class="col-auto">
-            <json-csv
-                :data="exportedData"
-                :labels="exportedLabel"
-                :name="exportedFilename"
-            >
-                <q-btn
-                    class="q-ma-xs"
-                    icon="cloud_download"
-                    label="Download CSV"
-                />
-            </json-csv>
-        </div>
+  <div class="row justify-between items-center" style="height: 150px">
+    <div class="col-auto">
+      <q-btn
+        class="q-ma-xs"
+        icon="delete"
+        color="negative"
+        label="Clear all data"
+        :disable="!units.length"
+        @click="clearStore()"
+      />
     </div>
+    <div class="col-auto">
+      <q-btn
+        class="q-ma-xs"
+        icon="stop"
+        color="primary"
+        label="Ingnore command"
+        @click="ignoreCommand()"
+      />
+    </div>
+    <div class="col-auto">
+      <q-toggle
+        v-model="timeCalibrationState"
+        label="Time Calibration"
+        class="q-ma-xs"
+      />
+    </div>
+    <div class="col-auto">
+      <q-toggle
+        v-model="combineCommand"
+        label="Combine Command"
+        class="q-ma-xs"
+      />
+    </div>
+    <div class="col-auto">
+      <json-csv
+        :data="exportedData"
+        :labels="exportedLabel"
+        :name="exportedFilename"
+      >
+        <q-btn class="q-ma-xs" icon="cloud_download" label="Download CSV" />
+      </json-csv>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -57,82 +53,82 @@ import moment from "moment";
 import { Report } from "../components/js/frame";
 
 export default {
-    // name: 'ComponentName',
-    components: {
-        JsonCsv,
+  // name: 'ComponentName',
+  components: {
+    JsonCsv
+  },
+  computed: {
+    ...mapState("database", ["units", "settings", "combineCmd"]),
+    ...mapGetters("database", ["selectedReports"]),
+    combineCommand: {
+      get() {
+        return this.combineCmd;
+      },
+      set(value) {
+        this.TOGGLE_COMBINE_CMD();
+      }
     },
-    computed: {
-        ...mapState("database", ["units", "settings", "combineCmd"]),
-        ...mapGetters("database", ["selectedReports"]),
-        combineCommand: {
-            get() {
-                return this.combineCmd;
-            },
-            set(value) {
-                this.TOGGLE_COMBINE_CMD();
-            },
-        },
-        timeCalibrationState: {
-            get() {
-                return this.settings.timeCalibration;
-            },
-            set(value) {
-                this.TOGGLE_TIME_CALIBRATION();
-            },
-        },
-        exportedData() {
-            return this.selectedReports.reverse().map(({ data }) =>
-                data
-                    .reverse()
-                    .filter(({ chartable }) => chartable)
-                    .reduce(
-                        (carry, { field, value, output, unit }) => ({
-                            ...carry,
-                            [field]: output,
-                        }),
-                        {}
-                    )
-            );
-        },
-        exportedLabel() {
-            return Report.reduce(
-                (carry, { field, title, unit }) => ({
-                    ...carry,
-                    [field]: title + (unit ? ` (${unit})` : ""),
-                }),
-                {}
-            );
-        },
-        exportedFilename() {
-            return `tracking-${moment().format("YYYYMMDDHHmmss")}.csv`;
-        },
+    timeCalibrationState: {
+      get() {
+        return this.settings.timeCalibration;
+      },
+      set(value) {
+        this.TOGGLE_TIME_CALIBRATION();
+      }
     },
-    methods: {
-        ...mapMutations("database", [
-            "TOGGLE_COMBINE_CMD",
-            "TOGGLE_TIME_CALIBRATION",
-        ]),
-        ...mapActions("database", ["RESET_DATABASE"]),
-        clearStore() {
-            this.$q
-                .dialog({
-                    title: "Confirmation",
-                    message: `Are you sure to remove all data?`,
-                    preventClose: true,
-                    cancel: true,
-                })
-                .then(() => {
-                    // clear all the store
-                    this.RESET_DATABASE();
-                    // reset command input
-                    this.$root.$emit("setCommand", "");
-                })
-                .catch(() => {});
-        },
-        ignoreCommand() {
-            this.$root.$emit("ignoreCommand");
-        },
+    exportedData() {
+      return this.selectedReports.reverse().map(({ data }) =>
+        data
+          .reverse()
+          .filter(({ chartable }) => chartable)
+          .reduce(
+            (carry, { field, value, output, unit }) => ({
+              ...carry,
+              [field]: output
+            }),
+            {}
+          )
+      );
     },
+    exportedLabel() {
+      return Report.reverse().reduce(
+        (carry, { field, title, unit }) => ({
+          ...carry,
+          [field]: title + (unit ? ` (${unit})` : "")
+        }),
+        {}
+      );
+    },
+    exportedFilename() {
+      return `tracking-${moment().format("YYYYMMDDHHmmss")}.csv`;
+    }
+  },
+  methods: {
+    ...mapMutations("database", [
+      "TOGGLE_COMBINE_CMD",
+      "TOGGLE_TIME_CALIBRATION"
+    ]),
+    ...mapActions("database", ["RESET_DATABASE"]),
+    clearStore() {
+      this.$q
+        .dialog({
+          title: "Confirmation",
+          message: `Are you sure to remove all data?`,
+          preventClose: true,
+          cancel: true
+        })
+        .then(() => {
+          // clear all the store
+          this.RESET_DATABASE();
+          // reset command input
+          this.$root.$emit("setCommand", "");
+        })
+        .catch(() => {});
+    },
+    ignoreCommand() {
+      this.$root.$emit("ignoreCommand");
+    }
+  }
 };
 </script>
 
