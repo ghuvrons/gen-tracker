07fff4ae9dfa9e9fd0ff4fd5a5b0676c16a58024
Pudja Mansyurin <pujakusumae@hotmail.com>
2021-02-07 17:35:18 +0700

    Add notification to pwa

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -3,30 +3,64 @@
  * when building for PRODUCTION
  */
 
-import { register } from 'register-service-worker'
+import { register } from "register-service-worker";
+import { Notify } from "quasar";
 
 register(process.env.SERVICE_WORKER_FILE, {
-  ready () {
-    console.log('App is being served from cache by a service worker.')
+  ready() {
+    console.log("App is being served from cache by a service worker.");
   },
-  registered (registration) { // registration -> a ServiceWorkerRegistration instance
-    console.log('Service worker has been registered.')
+  registered(registration) {
+    // registration -> a ServiceWorkerRegistration instance
+    console.log("Service worker has been registered.");
   },
-  cached (registration) { // registration -> a ServiceWorkerRegistration instance
-    console.log('Content has been cached for offline use.')
+  cached(registration) {
+    // registration -> a ServiceWorkerRegistration instance
+    console.log("Content has been cached for offline use.");
   },
-  updatefound (registration) { // registration -> a ServiceWorkerRegistration instance
-    console.log('New content is downloading.')
+  updatefound(registration) {
+    // registration -> a ServiceWorkerRegistration instance
+    console.log("New content is downloading.");
   },
-  updated (registration) { // registration -> a ServiceWorkerRegistration instance
-    console.log('New content is available; please refresh.')
+  updated(registration) {
+    // registration -> a ServiceWorkerRegistration instance
+    console.log("New content is available; please refresh.");
+
+    Notify.create({
+      message: "New content is available; please refresh.",
+      color: "red",
+      icon: "refresh",
+      actions: [
+        {
+          label: "Refresh",
+          color: "yellow",
+          handler: () => {
+            window.location.reload();
+          },
+        },
+        {
+          label: "Dismiss",
+          color: "white",
+          handler: () => {
+            /* ... */
+          },
+        },
+      ],
+    });
   },
-  offline () {
-    console.log('No internet connection found. App is running in offline mode.')
+  offline() {
+    console.log(
+      "No internet connection found. App is running in offline mode."
+    );
+
+    Notify.create({
+      message: "Offline mode",
+      color: "purple",
+    });
   },
-  error (err) {
-    console.error('Error during service worker registration:', err)
-  }
-})
+  error(err) {
+    console.error("Error during service worker registration:", err);
+  },
+});
 
 // ServiceWorkerRegistration: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
