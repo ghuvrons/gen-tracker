1deb7c15a9a0f62d83bdc8d863eeb874acb03bb7
Puja Kusuma <pujakusumae@hotmail.com>
2021-06-24 14:55:39 +0700

    Change domain

--- quasar.conf.js
+++ quasar.conf.js
@@ -192,7 +192,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: "com.genmotorcycles.tracker",
+        appId: "com.garda-energi.tracker",
       },
 
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
